775c6ba16b3cf3f61cd7249de046627d40ac7739
calin <calinoiu.alexandru@agilefreaks.com>
2014-04-07 15:20:55 +0300

    [#13] automaticly adds an acess token to a new client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,10 +3,15 @@ class Client
   include Mongoid::Timestamps
 
   field :secret, type: String, default: -> { SecureRandom.base64(64) }
+  field :name, type: String
 
   embeds_many :access_tokens
 
-  index({ 'access_token.token' => 1 }, { unique: true })
+  index 'access_tokens.token' => 1
+
+  before_create do
+    access_tokens.push(AccessToken.build)
+  end
 
   def self.verify(client_id)
     Client.where(id: client_id).first
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -6,4 +6,10 @@ describe Client do
   it { should embed_many(:access_tokens) }
 
   its(:secret) { should_not be_nil }
+
+  describe :before_create do
+    subject { Fabricate(:client) }
+
+    its('access_tokens.count') { should == 1 }
+  end
 end
