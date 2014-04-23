cb174eaeda77f53516b67c8b3b2c21d4ed550bf8
calin <calinoiu.alexandru@agilefreaks.com>
2014-04-23 16:31:18 +0300

    updates version number

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -4,6 +4,6 @@ class Configuration
     attr_accessor :google_api_key
   end
 
-  @app_version = '1.0.6'
+  @app_version = '1.0.7'
   @google_api_key = 'AIzaSyDiX6YE0kjKmnjSygNRC_sYq6MBUfzsg2I'
 end
