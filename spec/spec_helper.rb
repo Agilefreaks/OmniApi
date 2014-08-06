94485b6a54530900d8d5db5a383cd4dbeb5a9ca7
calin <calinoiu.alexandru@agilefreaks.com>
2014-08-06 14:55:04 +0300

    adds simple conv reporter for teamcity

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,6 @@ require 'rspec-spies'
 
 # Require all of the RSpec Support libraries
 Dir[File.expand_path(File.join('../support/**/*.rb'), __FILE__)].each { |f| require f }
+
+require 'simplecov'
+SimpleCov.start 'teamcity'
