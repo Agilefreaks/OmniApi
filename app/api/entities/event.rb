169566f9416e8e02a585b1d0e3b83b3b0b2a5e6e
adi <nistor.adrian@agilefreaks.com>
2014-08-13 22:44:05 +0300

    Content is only used for SMS events

--- app/api/entities/event.rb
+++ app/api/entities/event.rb
@@ -1,8 +1,9 @@
 module API
-  module Entities
-    class Event < Grape::Entity
-      expose :id, :identifier, :phone_number, :content
-      expose :_type, as: :type
-    end
+module Entities
+  class Event < Grape::Entity
+    expose :id, :identifier, :phone_number
+    expose :content, if: lambda { |obj, _options| obj.is_a? IncomingSmsEvent }
+    expose :_type, as: :type
   end
+end
 end
