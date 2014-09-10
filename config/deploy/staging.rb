190a50599655ff7f54f5377203431895bc1f9bba
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-10 11:20:06 +0300

    fixes rack env in deploy stages

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :deploy_to, '/var/www/omniapi'
 set :branch, 'staging'
 
-set :rails_env, 'staging'
+set :rack_env, 'staging'
 
 role :app, %w(deploy@46.16.191.69)
 role :web, %w(deploy@46.16.191.69)
