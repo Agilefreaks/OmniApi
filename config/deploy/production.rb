6cac43bbf025d3b10eab8ebc52594f081309a8d0
calin <calinoiu.alexandru@agilefreaks.com>
2014-09-15 16:31:03 +0300

    [oa-71] updates deployment files until the newrelicrpm PR's are solved

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :deploy_to, '/var/www/omniapi'
 set :branch, 'production'
 
-set :rack_env, 'production'
+set :rails_env, 'production'
 
 role :app, %w(deploy@5.10.81.84)
 role :web, %w(deploy@5.10.81.84)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :deploy_to, '/var/www/omniapi'
 set :branch, 'staging'
 
-set :rack_env, 'staging'
+set :rails_env, 'staging'
 
 role :app, %w(deploy@46.16.191.69)
 role :web, %w(deploy@46.16.191.69)
