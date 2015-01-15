ee160df49203bc32f56bcb04db6ebb24d64096df
Calin <calinoiu.alexandru@agilefreaks.com>
2015-01-15 11:24:01 +0200

    [oa-120] updates end call interaction

--- app/interactions/end_call.rb
+++ app/interactions/end_call.rb
@@ -1,14 +1,15 @@
 class EndCall
   def self.with(params)
-    EndCall.new(params[:access_token], params[:id]).execute
+    EndCall.new(params[:access_token], params[:id], params[:device_id]).execute
   end
 
-  attr_accessor :access_token, :id
+  attr_accessor :access_token, :id, :device_id
   attr_accessor :notification_service
 
-  def initialize(access_token, id)
+  def initialize(access_token, id, device_id)
     @access_token = access_token
     @id = id
+    @device_id = device_id
   end
 
   def execute
@@ -17,7 +18,11 @@ class EndCall
     @notification_service ||= NotificationService.new
 
     phone_call = user.phone_calls.find(@id)
-    @notification_service.end_call(phone_call, '')
+    phone_call.update_attribute(:state, :end_call)
+    @notification_service.notify(phone_call, @device_id)
+
+    # TODO: remove old method once the clients are updated
+    @notification_service.end_call(phone_call, @device_id)
 
     phone_call
   end
--- spec/interactions/end_call_spec.rb
+++ spec/interactions/end_call_spec.rb
@@ -6,7 +6,7 @@ describe :EndCall do
 
     let(:phone_call) { Fabricate(:phone_call, user: user) }
     let(:notification_service) { double(:notification_service) }
-    let(:end_call) { EndCall.new(access_token.token, phone_call.id) }
+    let(:end_call) { EndCall.new(access_token.token, phone_call.id, '42') }
 
     before do
       end_call.notification_service = notification_service
@@ -15,7 +15,8 @@ describe :EndCall do
     subject { end_call.execute }
 
     it 'will send a end_call notification with the correct params' do
-      expect(notification_service).to receive(:end_call).with(phone_call, '')
+      expect(notification_service).to receive(:notify).with(phone_call, '42')
+      expect(notification_service).to receive(:end_call).with(phone_call, '42')
       subject
     end
   end
