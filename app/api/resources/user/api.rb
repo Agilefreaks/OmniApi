f62ed1b15ae73b06f52bd4770c546896b3dac36d
Calin <calinoiu.alexandru@agilefreaks.com>
2015-01-15 16:01:06 +0200

    [oa-120] adds /user to fetch the authorizated user

--- app/api/resources/user/api.rb
+++ app/api/resources/user/api.rb
@@ -3,6 +3,12 @@ module API
     module User
       class Api < Grape::API
         resource :user do
+          desc 'Fetch user.', ParamsHelper.omni_headers
+          get do
+            authenticate!
+            present @current_user, with: API::Entities::User
+          end
+
           mount API::Resources::User::Contacts
 
           mount API::Resources::User::Devices
--- spec/api/resouces/user/api_spec.rb
+++ spec/api/resouces/user/api_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 describe API::Resources::User::Api do
   include_context :with_authenticated_client
 
+  describe "GET '/api/v1/user'" do
+    include_context :with_authenticated_user
+
+    before do
+      get '/api/v1/user', '', options
+    end
+
+    subject { last_response }
+
+    it { is_expected.to be_ok }
+  end
+
   describe :contacts do
     include_context :with_authenticated_user
 
