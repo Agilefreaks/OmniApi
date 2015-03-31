20674d11d7d378feb22b11a93924452dbaa19d01
calin <calinoiu.alexandru@agilefreaks.com>
2015-03-31 09:22:58 +0300

    reverts changes to the created at date"

--- app/factories/client_factory.rb
+++ app/factories/client_factory.rb
@@ -1,23 +1,22 @@
 class ClientFactory
   class << self
     def create_web_client(id = nil)
-      create_client(id, 'WebClient', :web_client)
+      create_client(id: id, name: 'WebClient', group: :web_client)
     end
 
     def create_android_client(id = nil)
-      create_client(id, 'AndroidClient', :android_client)
+      create_client(id: id, name: 'AndroidClient', group: :android_client)
     end
 
     def create_win_client(id = nil)
-      create_client(id, 'WinClient', :win_client)
+      create_client(id: id, name: 'WinClient', group: :win_client)
     end
 
-    private
-
-    def create_client(id = nil, name, group)
+    # rubocop:disable AbcSize
+    def create_client(id: nil, name:, group: nil)
       client = ::Client.new do |c|
         c.name = name
-        c._id = id
+        c._id = id unless id.nil?
       end
       access_token = AccessToken.build
       access_token.expires_at = Time.now.utc + 1.year
