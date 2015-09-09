b678550822db7f31dd97b8f921a08db0f1ead3be
Cristi Badila <cristi.badila@gmail.com>
2015-09-09 14:29:54 +0300

    [#164] removes the access token info from the user entity

--- spec/api/entities/user_spec.rb
+++ spec/api/entities/user_spec.rb
@@ -25,28 +25,4 @@ describe API::Entities::User do
       it { is_expected.to eq user.contacts_updated_at.iso8601 }
     end
   end
-
-  describe 'access_token' do
-    subject { entity['access_token'] }
-
-    context 'with a client_id' do
-      include_context :with_authenticated_client
-
-      let(:options) { { client_id: client.id } }
-
-      context 'and a active token for that client' do
-        let!(:access_token) { GenerateOauthToken.build_access_token_for(user, client.id) }
-
-        it { is_expected.to eq access_token.token }
-      end
-    end
-
-    context 'with no client_id' do
-      context 'and a active token' do
-        let!(:access_token) { GenerateOauthToken.build_access_token_for(user, '42') }
-
-        it { is_expected.to eq access_token.token }
-      end
-    end
-  end
 end
