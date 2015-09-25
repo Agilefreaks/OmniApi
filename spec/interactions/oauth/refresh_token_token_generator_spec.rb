ed2c8a7fe673f3356cf2c063cda8ca04d715c12e
Cristi Badila <cristi.badila@gmail.com>
2015-09-25 12:20:37 +0300

    [#170] removes duplication of user client association access token
    This is a combination of 5 commits.
    [#170] modifies the refresh_token_token_generator so as to work only for
    user tokens
    
    [#170] modifies the user_client_association model so that instead of
    embedding an association token it uses a user access_token
    
    [#170] updates the create_user_client_association interaction so as to
    not create 2 tokens when creating a user client association
    
    [#170] updates spec to reflect the fact the user_client_associations
    don't have an embedded access token any more
    
    [#170] updates the users api so as to use the updated
    RefreshTokenTokenGenerator

--- spec/interactions/oauth/refresh_token_token_generator_spec.rb
+++ spec/interactions/oauth/refresh_token_token_generator_spec.rb
@@ -4,39 +4,42 @@ describe Oauth::RefreshTokenTokenGenerator do
   let(:client) { Fabricate(:client, secret: 'secret') }
   let(:access_token) { AccessToken.build('42', token: 'access') }
   let(:refresh_token) { RefreshToken.build(token: 'refresh') }
-  let(:params) { {} }
-  let(:req) { double('request', refresh_token: refresh_token.token, params: params) }
+  let(:req) { double('request', refresh_token: refresh_token.token) }
+  let(:user) { Fabricate(:user) }
 
   describe '.generate' do
     subject { Oauth::RefreshTokenTokenGenerator.generate(client, req) }
 
-    context 'when resource is not present' do
-      before { params['resource_type'] = '' }
-
-      it 'delegates generate to the RefreshTokenUserTokenGenerator' do
-        expect(Oauth::RefreshTokenUserTokenGenerator).to receive(:generate).with(client, req)
-
-        subject
+    context 'when refresh token is valid' do
+      before :each do
+        access_token.refresh_token = refresh_token
+        user.access_tokens.push(access_token)
+        user.save
       end
+
+      it 'will not create a new access token' do
+        expect { subject }.to change(user.access_tokens, :count).by(0)
+      end
+
+      context 'if access_token is expired' do
+        before do
+          access_token.expires_at = Time.now.utc - 2.months
+          user.save
+        end
+
+        its(:expires_at) { should >= Time.now.utc }
+      end
+
+      its(:client_id) { should == '42' }
+
+      its('refresh_token.token') { should eq 'refresh' }
     end
 
-    context 'when resource is user' do
-      before { params['resource_type'] = 'user' }
+    context 'when refresh token is invalid' do
+      it 'invalidates the request' do
+        expect(req).to receive(:invalid_grant!).and_raise(Rack::OAuth2::Server::Resource::BadRequest)
 
-      it 'delegates generate to the RefreshTokenUserTokenGenerator' do
-        expect(Oauth::RefreshTokenTokenGenerator).to receive(:generate).with(client, req)
-
-        subject
-      end
-    end
-
-    context 'when resource is user_client_association' do
-      before { params['resource_type'] = 'user_client_association' }
-
-      it 'delegates generate to the RefreshTokenUserClientAssociationTokenGenerator' do
-        expect(Oauth::RefreshTokenUserClientAssociationTokenGenerator).to receive(:generate).with(client, req)
-
-        subject
+        expect { subject }.to raise_error Rack::OAuth2::Server::Resource::BadRequest
       end
     end
   end
