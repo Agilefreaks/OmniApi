6a459d976ce7f846640f7d642cb7ba44a9e0dce9
Cristi Badila <cristi.badila@gmail.com>
2015-09-25 12:20:37 +0300

    [#170] modifies the refresh_token_token_generator so as to work only for user tokens

--- app/interactions/oauth/refresh_token_user_client_association_token_generator.rb
+++
@@ -1,8 +0,0 @@
-module Oauth
-  class RefreshTokenUserClientAssociationTokenGenerator < BaseRefreshTokenTokenGenerator
-    def self.find_token(req)
-      user_client_association = UserClientAssociation.find_by_token(req.refresh_token)
-      user_client_association.access_token if user_client_association
-    end
-  end
-end
